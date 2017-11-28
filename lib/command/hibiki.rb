61f0b14c93bd335815a78bd78b9907c70af9b3a8
KoellM <hxtx2013@gmail.com>
2017-11-28 18:17:22 +0800

    增加url

--- lib/command/hibiki.rb
+++ lib/command/hibiki.rb
@@ -9,8 +9,7 @@ class Hibiki
         end
         begin
             radio_name = a
-            res = self.get_api("https://vcms-api.hibiki-radio.jp/api/v1/programs/#{radio_name}")
-            infos = JSON.parse(res.body)
+            infos = self.get_api("https://vcms-api.hibiki-radio.jp/api/v1/programs/#{radio_name}")
             episode_id = infos["episode"]["video"]["id"]
           rescue => e
             puts e.message
@@ -21,7 +20,9 @@ class Hibiki
             additional_episode_id = nil
           end
           begin
-            BotMessageSender.new(bot).send_message("[Hibiki] 搜索:#{radio_name} 结果: \n #{infos["episode"]["program_name"]} #{infos["episode"]["name"]}(#{infos["episode"]["updated_at"]})\n#{infos["description"]}\n本体ID: #{episode_id}, #{unless (additional_episode_id.nil?) then "楽屋裏ID: #{additional_episode_id}" end }")
+            url = self.get_api("https://vcms-api.hibiki-radio.jp/api/v1/videos/play_check?video_id=#{episode_id}")["playlist_url"]
+            additional_url = self.get_api("https://vcms-api.hibiki-radio.jp/api/v1/videos/play_check?video_id=#{additional_episode_id}")["playlist_url"]
+            BotMessageSender.new(bot).send_message("[Hibiki] 搜索:#{radio_name} 结果: \n #{infos["episode"]["program_name"]} #{infos["episode"]["name"]}(#{infos["episode"]["updated_at"]})\n#{infos["description"]}\n本体ID: #{episode_id}(#{url}), #{unless (additional_episode_id.nil?) then "楽屋裏ID: #{additional_episode_id}(#{additional_url})" end }")
           rescue => e
             puts e.message
           end
@@ -30,12 +31,13 @@ class Hibiki
     def self.get_api(url)
         @a = Mechanize.new
         @a.user_agent_alias = 'Windows Chrome'
-        @a.get(
+        res = @a.get(
             url,
             [],
             "http://hibiki-radio.jp/",
             'X-Requested-With' => 'XMLHttpRequest',
             'Origin' => 'http://hibiki-radio.jp'
         )
+        return JSON.parse(res.body)
     end
 end
