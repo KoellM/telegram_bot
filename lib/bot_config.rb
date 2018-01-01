7447dac09e089a90b8fa84fde8e36bf4618aaf59
KoellM <hxtx2013@gmail.com>
2018-01-02 02:24:11 +0800

    配置文件

--- lib/bot_config.rb
+++ lib/bot_config.rb
@@ -14,4 +14,16 @@ class BotConfig
     def token
         YAML::load(IO.read('config/config.yml'))['token']
     end
+
+    def self.qiniu_app_access_key
+        YAML::load(IO.read('config/config.yml'))['qiniu_app_access_key']
+    end
+
+    def self.qiniu_app_secret_key
+        YAML::load(IO.read('config/config.yml'))['qiniu_app_secret_key']
+    end
+
+    def self.qiniu_app_bucket
+        YAML::load(IO.read('config/config.yml'))['qiniu_app_bucket']
+    end        
 end
